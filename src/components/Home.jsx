fb5dd00184a90a12adb4d95f6e63416737107829
Kamal Nayan <sknasylum8454@gmail.com>
2023-10-30 23:48:31 +0530

    saka push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import shoppingSVG from "./shopping.svg";
 
+import { motion } from "framer-motion";
+
 const Home = () => {
   return (
-    <div className="container">
+    <motion.div
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      transition={{
+        duration: 1,
+        ease: "linear",
+        type: "spring",
+        stiffness: 300,
+      }}
+      className="container"
+    >
       <div className="left">
         <h1 className="intro-title">
           THE <span>Grocery Store</span>
         </h1>
-        
       </div>
       <div className="right">
-        <img className="svg" src={shoppingSVG} height="450px" width="450px" /> {/* Used the correct variable name */}
+        <img className="svg" src={shoppingSVG} height="450px" width="450px" />{" "}
+        {/* Used the correct variable name */}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
